eedb82031cd6e29f8c79a64a9f6e9dc4a464952b
beaudario <dario.douma@upcmail.nl>
2021-11-03 14:59:45 +0100

    Added Services

---
+++ Services/AfspraakService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_ccsb.Services
+{
+    public class AfspraakService: IAfspraakService
+    {
+    }
+}
