145fc7dcf265f6dade771c0de1f004521515f4cd
Quint Leusink <0308410@student.rocvantwente.nl>
2021-11-25 10:19:55 +0100

    .

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project_ccsb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_ccsb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Utility/Helper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_ccsb.Utility
+{
+    public static class Helper
+    {
+        public static readonly string Admin = "Beheerder";
+        public static readonly string Klant = "Klant";
+        public static readonly string Medewerker = "Medewerker";
+
+        public static List<SelectListItem> GetRolesForDropDown(bool isAdmin)
+        {
+            var items = new List<SelectListItem>
+            {
+                new SelectListItem{Value=Helper.Admin , Text = Helper.Admin},
+                new SelectListItem{Value=Helper.Klant , Text = Helper.Klant},
+                new SelectListItem{Value=Helper.Medewerker , Text = Helper.Medewerker}
+            };
+            return items.OrderBy(s => s.Text).ToList();
+        }
+    }
+}
